99da95ad8390fca10bc11622d21ab6a487a32d61
Therese Levin <therese.levin@outlook.com>
2018-10-13 17:14:27 +0200

    Js for all color change and new HTML form for text input

--- public_html/js/customize.js
+++ public_html/js/customize.js
@@ -1,16 +1,14 @@
 /* --- JAVASCRIPT FOR CHANGING COLORS AND IMAGES --- */
 
-let defaultColor = "#444444";
+
 
 window.addEventListener("load", startUp, false);
 
 function startUp() {
     bColor = document.querySelector("#backg-color");
     hColor = document.querySelector("#h1-color");
-
-    //lägger till defaultfärgen
-    bColor.value = defaultColor;
-    hColor.value = "#993999";
+    uColor = document.querySelector("#u-color");
+    mColor = document.querySelector("#m-color");
 
     //lyssnar efter event, kör function
     bColor.addEventListener("input", updateFirst, false);
@@ -18,6 +16,12 @@ function startUp() {
 
     hColor.addEventListener("input", updateFirst2, false);
     hColor.addEventListener("change", updateAll2, false);
+
+    uColor.addEventListener("input", updateFirst3, false);
+    uColor.addEventListener("change", updateAll3, false);
+
+    mColor.addEventListener("input", updateFirst4, false);
+    mColor.addEventListener("change", updateAll4, false);
 
     //först markerad
     bColor.select();
@@ -46,6 +50,32 @@ function updateFirst2(event) {
 function updateAll2(event) {
   document.querySelectorAll("h3").forEach(function(menyHeading) {
     menyHeading.style.color = event.target.value;
+  });
+}
+
+function updateFirst3(event) {
+  let underHeading = document.querySelector("small");
+  if (underHeading) {
+    underHeading.style.color = event.target.value;
+  }
+}
+
+function updateAll3(event) {
+  document.querySelectorAll("small").forEach(function(underHeading) {
+    underHeading.style.color = event.target.value;
+  });
+}
+
+function updateFirst4(event) {
+  let item = document.querySelector(".item");
+  if (item) {
+    item.style.color = event.target.value;
+  }
+}
+
+function updateAll4(event) {
+  document.querySelectorAll(".item").forEach(function(item) {
+    item.style.color = event.target.value;
   });
 }
 
